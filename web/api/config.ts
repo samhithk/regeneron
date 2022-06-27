6d767a27c80870f471d7e10dd98d43ea2325c1d8
Samhith Kotagiri <samhith.kotagiri@mounge.com>
2022-06-27 14:07:27 -0400

    fix: api url

--- web/api/config.ts
+++ web/api/config.ts
@@ -1,4 +1,4 @@
 export const BASE_API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://regeneron-samhithk.vercel.app//api/v1"
+    ? "https://regeneron-samhithk.vercel.app/api/v1"
     : "http://localhost:3000/api/v1";
